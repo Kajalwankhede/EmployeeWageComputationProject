fec8aa55ae5dbbc42697337c650c5a72fbb9e54f
Kajakwankhede <Kajalwankhede@example.com>
2022-10-29 16:57:25 +0530

    Added Employee Wage using Switch case

--- src/com/empwage/EmployeeWageComputation.java
+++ src/com/empwage/EmployeeWageComputation.java
@@ -1,21 +1,30 @@
 package com.empwage;
 
 public class EmployeeWageComputation {
+    static final int fullTime = 1;
+    static final int partTime=2;
+    static final int empRatePerHour=20;
 
     public static void main(String[] args) {
-        int fullTime = 1;
-        int empRatePerHour=1;
         int empHour=0;
         int empWage=0;
         System.out.println("\n******** Welcome To Employee Wage Computation ********");
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == fullTime) {
-            System.out.println("Employee Present!");
-        } else {
-            System.out.println("Employee Absent!");
+        int Check = (int) Math.floor(Math.random()*10) % 3;
+        switch (Check) {
+            case partTime:
+                empHour = 4;
+                System.out.println("Employee is Part time.");
+                break;
 
+            case fullTime:
+                empHour = 8;
+                System.out.println("Employee is Full time.");
+                break;
+            default:
+                System.out.println("Em");
+                break;
         }
-        empWage=empHour*empRatePerHour;
+            empWage=empHour*empRatePerHour;
         System.out.println("Employee Daily Wage is: "+empWage);
     }
 }
