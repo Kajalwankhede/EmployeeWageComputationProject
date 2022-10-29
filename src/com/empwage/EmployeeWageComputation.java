2fc2a097ac0ed48601386308084bd4fab1c2bba6
Kajakwankhede <Kajalwankhede@example.com>
2022-10-29 17:24:44 +0530

    Added Calculate Employee wage till total Working Hour Per month reached

--- src/com/empwage/EmployeeWageComputation.java
+++ src/com/empwage/EmployeeWageComputation.java
@@ -5,13 +5,16 @@ public class EmployeeWageComputation {
     static final int partTime=2;
     static final int empRatePerHour=20;
     static final int numOfWorkingDays=2;
+    static final int maxHourMonth=10;
 
     public static void main(String[] args) {
         int empHour = 0;
         int empWage = 0;
-        int totalEmpWage = 0;
+        int totalEmpHour=0;
+        int totalWorkingDays=0;
         System.out.println("\n******** Welcome To Employee Wage Computation ********");
-        for (int day = 0; day < numOfWorkingDays; day++) {
+       while(totalEmpHour <= maxHourMonth && totalWorkingDays<numOfWorkingDays){
+           totalWorkingDays++;
             int Check = (int) Math.floor(Math.random() * 10) % 3;
             switch (Check) {
                 case partTime:
@@ -24,14 +27,16 @@ public class EmployeeWageComputation {
                     System.out.println("Employee is Full time.");
                     break;
                 default:
-                    System.out.println("Employee Absent");
                     break;
             }
+            totalEmpHour+=empHour;
+           System.out.println("Total Working Days: "+totalWorkingDays+ " Employee Hours: "+empHour);
             empWage = empHour * empRatePerHour;
-            totalEmpWage+=empWage;
-            System.out.println("Employee Daily Wage is: " + empWage);
+
         }
-        System.out.println("Total Emp Wage:"+totalEmpWage);
+
+        int totalEmpWage=totalEmpHour*empRatePerHour;
+        System.out.println("Total Employee Wage is: " + totalEmpWage);
     }
 }
 
