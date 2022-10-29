9343509f2477eba36d73a093c911ea5aaf3fbe04
Kajakwankhede <Kajalwankhede@example.com>
2022-10-29 17:34:55 +0530

    Added to check employee is part time or full

--- src/com/empwage/EmployeeWageComputation.java
+++ src/com/empwage/EmployeeWageComputation.java
@@ -1,42 +1,30 @@
 package com.empwage;
 
 public class EmployeeWageComputation {
-    static final int fullTime = 1;
-    static final int partTime=2;
-    static final int empRatePerHour=20;
-    static final int numOfWorkingDays=2;
-    static final int maxHourMonth=10;
-
     public static void main(String[] args) {
+        int fullTime = 1;
+        int partTime = 2;
         int empHour = 0;
+        int empRatePerHour=20;
         int empWage = 0;
-        int totalEmpHour=0;
-        int totalWorkingDays=0;
         System.out.println("\n******** Welcome To Employee Wage Computation ********");
-       while(totalEmpHour <= maxHourMonth && totalWorkingDays<numOfWorkingDays){
-           totalWorkingDays++;
-            int Check = (int) Math.floor(Math.random() * 10) % 3;
-            switch (Check) {
-                case partTime:
-                    empHour = 4;
-                    System.out.println("Employee is Part time.");
-                    break;
+        double Check = (int) Math.floor(Math.random() * 10) % 3;
+        if (Check == partTime) {
+            System.out.println("Employee is Part time.");
+            empHour = 4;
+        }
+                 else if (Check == fullTime) {
+            System.out.println("Employee is Full time.");
+            empHour = 8;
+        }
+                 else{
+        empHour = 0;
+    }
+          empWage = empHour * empRatePerHour;
+           System.out.println("Employee Wage is: "+empWage);
 
-                case fullTime:
-                    empHour = 8;
-                    System.out.println("Employee is Full time.");
-                    break;
-                default:
-                    break;
-            }
-            totalEmpHour+=empHour;
-           System.out.println("Total Working Days: "+totalWorkingDays+ " Employee Hours: "+empHour);
-            empWage = empHour * empRatePerHour;
 
         }
 
-        int totalEmpWage=totalEmpHour*empRatePerHour;
-        System.out.println("Total Employee Wage is: " + totalEmpWage);
-    }
 }
 
