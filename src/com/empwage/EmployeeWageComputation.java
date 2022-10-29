755c6ba5595b220ce252d555dabed3c34e8f6b51
Kajakwankhede <Kajalwankhede@example.com>
2022-10-29 16:37:36 +0530

    Added To check Daily employee wage computation

--- src/com/empwage/EmployeeWageComputation.java
+++ src/com/empwage/EmployeeWageComputation.java
@@ -4,6 +4,9 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         int fullTime = 1;
+        int empRatePerHour=1;
+        int empHour=0;
+        int empWage=0;
         System.out.println("\n******** Welcome To Employee Wage Computation ********");
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == fullTime) {
@@ -12,6 +15,8 @@ public class EmployeeWageComputation {
             System.out.println("Employee Absent!");
 
         }
+        empWage=empHour*empRatePerHour;
+        System.out.println("Employee Daily Wage is: "+empWage);
     }
 }
 
